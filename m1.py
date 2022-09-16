1627e004d1e3e8e95ae7570bd458d73dd6dccf0f
Байбак Кирилл <zloifox@gmail.com>
2022-09-16 18:46:45 +0300

    before class convertation

--- m1.py
+++ m1.py
@@ -11,9 +11,10 @@ TOKEN = os.getenv('ACCESS_TOKEN')
 GROUP_ID = os.getenv('GROUP_ID')
 
 vk_session = vk_api.VkApi(token=TOKEN)
-session_api = vk_session.get_api()
+long_poll = VkBotLongPoll(vk_session, GROUP_ID)
 
-longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+# Вызов методов vk_api
+session_api = vk_session.get_api()
 
 
 def send_message(session_api, peer_id, message=None,
@@ -25,26 +26,45 @@ def send_message(session_api, peer_id, message=None,
     )
 
 
+def get_user_name(user_id):
+    return vk_session.users.get(user_id=user_id)[0].get('first_name')
+
+
+def get_user_city(user_id):
+    return vk_session.users.get(user_id=user_id, fields="city")[0].get('city').get('title')
+
+
 # while True:
-for event in longpoll.listen():
-    print('1 ->', event)
-    print('1.1 ->', event.obj)
+for event in long_poll.listen():
+    obj = event.obj
+    print('Main_event ->', event)
+    print('1 ->', obj)
+    # print('Else - 2 ->', vk_session.method('users.get', {'user_ids': obj.message.get("from_id")}))
     if event.type == VkBotEventType.MESSAGE_NEW:
-        print('Chat-2 ->', event.obj.get('peer_id'))
-        print('Chat-1 ->', event.message.get('peer_id'))
+        user_name = vk_session.method(
+            'users.get', {'user_ids': obj.message.get(
+                "from_id")})[0].get('first_name')
+        user_city = vk_session.method(
+            'users.get', {'user_ids': obj.message.get(
+                "from_id"), 'fields': 'city'})[0].get('city').get('title')
+        print('Имя ->', user_name)
+        print('Город ->', user_city)
         print("В чате появилось новое сообщение!")
         send_message(
             session_api,
-            peer_id=event.message.get('peer_id'),
-            # message='Привет, {0}!'.format(last_and_first_name))
-            message=f'Привет - Я бот, проверка связи ID >>'
-                    f'{event.message.get("from_id")}')
+            peer_id=obj.message.get('peer_id'),
+            message=f'Привет - Я бот, проверка связи. Ваш ID >> '
+                    f'{obj.message.get("from_id")}\n'
+                    f'Ваше имя: {user_name}, '
+                    f'город {user_city}'
+        )
+        print('11->', get_user_name(obj.message.get("from_id")))
+        print('22->', get_user_city(obj.message.get("from_id")))
     if event.type == VkBotEventType.WALL_POST_NEW:
         print('На стене появилась новая запись')
         send_message(
             session_api,
-            peer_id=event.object.from_id,
-            # message='Привет, {0}!'.format(last_and_first_name))
+            peer_id=obj.from_id,
             message='Привет - Я бот, проверка связи!!!')
-    # if event.type == VkBotEventType.LIKE_ADD:
-    #     print('Лайкнули запись!')
+    else:
+        print('Else type ->', event.type)
